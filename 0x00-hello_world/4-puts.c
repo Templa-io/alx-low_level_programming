ca938ca0d9d4a111c1e196a77b92d8c83af3ab02
Templa-io <humphreywritter@gmail.com>
2022-10-15 03:17:05 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /*
  * main - print a line of code using puts
  *
- * Return: 0
+ * Return:0
  */
-int main (void)
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 		return (0);
 }
