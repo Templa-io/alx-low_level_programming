557b50c458c720efa1e43e0703bd1bd9fbaff8c0
Templa-io <humphreywritter@gmail.com>
2022-10-15 04:34:23 -0700

    Update 6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %lu bytes(S)\n", (unsigned long)sizeof(a));
-	printf("Size of a int: %lu bytes(S)\n", (unsigned long)sizeof(b));
+	printf("Size of an int: %lu bytes(S)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu bytes(S)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu bytes(S)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu bytes(S)\n", (unsigned long)sizeof(f));
