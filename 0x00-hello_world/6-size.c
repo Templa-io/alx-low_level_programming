2ba78d75f01dde3ec7faeafc54f305e07f116d32
Templa-io <humphreywritter@gmail.com>
2022-10-15 06:49:19 -0700

    Update 100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
+/*
+ * File: 101-quote.c
+ */
+
+#include <unistd.h>
+
 /**
- * main - print out size of data types in C
- * code by bwave ict
- * Return: 0
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
-	printf("Size of a char: %u bytes(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %u bytes(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %u bytes(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %u bytes(s)\n", (unsigned long)sizeof(f));
-	return (0);
+	return (1);
 }
