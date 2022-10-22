1459a5a6330b904bfa8acaa875dd12e684671681
Templa-io <humphreywritter@gmail.com>
2022-10-22 02:12:51 -0700

    Update 0-isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Write a function that checks for uppercase character.
+ * @c: input
+ * Returns 1 if c is uppercase, 0 otherwis
+ */
+
+int _isupper(int c)
+{
+	if(c >= 'A' && c <= 'Z')
+		return(1);
+
+	else
+
+		return(0);
+	
+
+}
