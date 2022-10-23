df7b7f22c51c07c3564018d11d326cc2effe98e2
Templa-io <humphreywritter@gmail.com>
2022-10-23 04:04:03 -0700

    update 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* more_numbers - prints 10x numbers from 0-14
+* Return: numbers from 0-14
+*/
+
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+
+	}
+	_putchar('\n');
+}
