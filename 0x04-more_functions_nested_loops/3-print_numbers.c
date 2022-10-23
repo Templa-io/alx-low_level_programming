bcf5a07a4e4878cdf099f50cdaaf8746d2b6442a
Templa-io <humphreywritter@gmail.com>
2022-10-23 01:31:20 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_most_numbers - prints the numbers, from 0-9, followed by a \n.
+ * print_numbers - prints the numbers, from 0-9, followed by a \n.
  * @void:
  * Return: numbers but 2 and 4
  */
@@ -12,14 +12,7 @@ void print_most_numbers(void)
 	int i;
 
 	for (i = 0; i <= 9; i++)
-	{
-		if ((i == 2) || (i == 4))
-
-			continue;
-		else
-			_putchar(i + '0');
-	}
-
-	putchar('\n')
+		_putchar(i + '0');
+	_putchar('\n');
 }
 
