58898ad1b4c5b97287da4896535e6adef9b027fd
Templa-io <humphreywritter@gmail.com>
2022-10-24 17:38:17 -0700

    update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,13 +5,6 @@
  *
  * Return: Always 0.
  */
-#include <stdio.h>
-
-/**
- * main - prints the largest prime factor of 612852475143
- *
- * Return: always 0
- */
 int main(void)
 {
 	unsigned long int i, n = 612852475143;
