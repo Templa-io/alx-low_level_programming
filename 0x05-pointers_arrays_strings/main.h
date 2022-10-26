d4811bc61b242ddb9b7232c1a8357873b1c23b58
Templa-io <humphreywritter@gmail.com>
2022-10-26 03:42:40 -0700

    Update 1-swap.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+#ifndef RESET_TO_98_H
+#define RESET_TO_98_H
+void reset_to_98(int *n);
+#endif
+
+#ifndef SWAP_INT_H
+#define SWAP_INT_H
+void swap_int(int *a, int *b);
+#endif
+
+#ifndef _STRLEN_H
+#define _STRLEN_H
+int _strlen(char *s);
+#endif
+
+#ifndef _PUTS_H
+#define _PUTS_H
+void _puts(char *str);
+#endif
+
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+int _putchar(char c);
+#endif
+
+#ifndef PRINT_REV_H
+#define _PRINT_REV_H
+void print_rev(char *s);
+#endif
+
+#ifndef _REV_STRING_H
+#define _REV_STRING_H
+void rev_string(char *s);
+#endif
+
+#ifndef _PUTS2_H
+#define _PUTS2_H
+void puts2(char *str);
+#endif
+
+#ifndef PUTS_HALF_H
+#define PUTS_HALF_H
+void puts_half(char *str);
+#endif
+
+#ifndef PRINT_ARRAY_H
+#define PRINT_ARRAY_H
+void print_array(int *a, int n);
+#endif
+
+#ifndef STRCPY_H
+#define STRCPY_H
+char *_strcpy(char *dest, char *src);
+#endif
+
+#ifndef _ATOI_H
+#define _ATOI_H
+int _atoi(char *s);
+#endif
