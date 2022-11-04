568fc9522ffa2d5ff85b92bf98f69c15df5b3b48
Templa-io <humphreywritter@gmail.com>
2022-11-03 22:27:44 -0700

    Update 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+void _print_rev_recursion(char *s)
+{
+
+
+ /*Base case: check if it end of string and print new line character */
+    if (*s == '\0'){
+      _putchar('\n');
+    }
+    else
+    {
+      /* print the next character */
+      _print_rev_recursion(++s);
+      /* print the current character */
+      _putchar(*s);
+    }
+
+  /* print character (*) then I will print *s++ */
+/* I want to print *s++ before I print s */
+
+
+
+}
