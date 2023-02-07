119efd501bc03fb773d6fe41fdfe537d375ebb38
Templa-io <humphreywritter@gmail.com>
2023-02-07 07:35:35 -0800

    update file

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Implements the djb2 algorithm
+ * @str: A string
+ * Return: Hash representation of @str as an unsigned long int
+ */
+
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
@@ -9,4 +18,4 @@ unsigned long int hash_djb2(const unsigned char *str)
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
     }
     return (hash);
-}
+i}
